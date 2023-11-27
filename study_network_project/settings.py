bb7ae5e5dc338936c8e75139d8ebb1a1227ccd18
nicknetic <Nikagagua@live.com>
2023-11-27 13:24:11 +0400

    Updated Password reset function

--- study_network_project/settings.py
+++ study_network_project/settings.py
@@ -95,7 +95,7 @@ LOGIN_URL = 'login'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SMTP Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
